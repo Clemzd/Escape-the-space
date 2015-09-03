24ad22fdf31c6101f74b0721784fdf4013ad4590
Clemzd <clement.guet@gmail.com>
2015-09-03 21:45:52 +0200

    removed unused files

--- js/main.js
+++
@@ -1,9 +0,0 @@
-var width = window.innerWidth - 20;
-var height = window.innerHeight - 20;
-
-var game = new Phaser.Game(width, height, Phaser.AUTO, '');
-
-console.log(State1);
-
-//game.state.add('state1', State1);
-// game.state.start('state1');
